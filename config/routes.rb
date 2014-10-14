27cdc8fd7603d43d1e28fd02c2390967fea491ed
diplay <diplaySuper@gmail.com>
2014-10-14 19:30:50 +0400

    facebook omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 PartySurfing::Application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: "users/omniauth_callbacks"}
+
   devise_for :admin_users, ActiveAdmin::Devise.config
+
   ActiveAdmin.routes(self)
 
   resources :parties do
