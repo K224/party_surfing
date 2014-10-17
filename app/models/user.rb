6d56e18994a70c663c99de10207689a8a44576ab
diplay <diplaySuper@gmail.com>
2014-10-17 11:02:23 +0400

    fixed facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+      if data = session["devise.facebook_data"]["info"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
         user.profile.update(name: data["first_name"], lastname: data["last_name"])
       end
