0fec5cc2ea7780d9d12a72b0f7270360419c8786
Alzater <alzater@mail.ru>
2014-10-25 00:00:16 +0400

    ckeditor fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ PartySurfing::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.precompile += ['ckeditor/*']
 end
