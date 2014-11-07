eff8ac297aa6c844563f376e6dd1ddb07c5b5b7f
diplay <diplaySuper@gmail.com>
2014-11-07 23:15:38 +0300

    vk auth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,6 +234,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  config.omniauth :vkontakte, ENV['VK_KEY'], ENV['VK_SECRET'], scope: 'email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
