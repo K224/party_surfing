112ccf92fa5c72524bacd00a215bb1ca4315c2d9
avitella <avitella@yandex-team.ru>
2014-12-13 02:16:06 +0300

    added tests

--- test/controllers/registration_controller_test.rb
+++ test/controllers/registration_controller_test.rb
@@ -28,4 +28,70 @@ class RegistrationsControllerTest < ActionController::TestCase
     assert has_content? "Email can't be blank"
     assert has_content? "Password can't be blank"
   end
+
+  test "signup without password" do
+    visit "/users/sign_up"
+    fill_in "Email", with: "hello@world.com"
+    click_on "Sign up"
+    assert_equal "/users", current_path
+    assert has_content? "2 errors prohibited this user from being saved:"
+    assert has_content? "Profile birthday is incorrect"
+    assert has_content? "Password can't be blank"
+  end
+
+  test "signup without email" do
+    visit "/users/sign_up"
+    fill_in "Password", with: "hello"
+    click_on "Sign up"
+    assert_equal "/users", current_path
+    assert has_content? "4 errors prohibited this user from being saved:"
+    assert has_content? "Profile birthday is incorrect"
+    assert has_content? "Email can't be blank"
+    assert has_content? "Password confirmation doesn't match Password"
+    assert has_content? "Password is too short (minimum is 8 characters)"
+  end
+
+  test "signup with short password" do
+    visit "/users/sign_up"
+    fill_in "Email", with: "hello@world.com"
+    fill_in "Password", with: "hello"
+    fill_in "Password confirmation", with: "hello"
+    fill_in "Birthday", with: "01-01-1992"
+    click_on "Sign up"
+    assert_equal "/users", current_path
+    assert has_content? "1 error prohibited this user from being saved:"
+    assert has_content? "Password is too short (minimum is 8 characters)"
+  end
+
+  test "signup with wrong password confirmation" do
+    visit "/users/sign_up"
+    fill_in "Email", with: "hello@world.com"
+    fill_in "Password", with: "helloworld"
+    fill_in "Password confirmation", with: "helloworld123"
+    fill_in "Birthday", with: "01-01-1992"
+    click_on "Sign up"
+    assert_equal "/users", current_path
+    assert has_content? "1 error prohibited this user from being saved:"
+    assert has_content? "Password confirmation doesn't match Password"
+  end
+
+  test "signup ok" do
+    visit "/users/sign_up"
+    fill_in "Email", with: "hello@world.com"
+    fill_in "Password", with: "helloworld"
+    fill_in "Password confirmation", with: "helloworld"
+    fill_in "Birthday", with: "01-01-1992"
+    click_on "Sign up"
+    assert_equal "/", current_path
+    assert has_link? "Выйти"
+    assert has_link? "Профиль"
+    click_on "Выйти"
+    click_on "Вход"
+    fill_in "Email", with: "hello@world.com"
+    fill_in "Password", with: "helloworld"
+    click_on "Log in"
+    assert_equal "/", current_path
+    assert has_link? "Выйти"
+    assert has_link? "Профиль"
+  end
 end
