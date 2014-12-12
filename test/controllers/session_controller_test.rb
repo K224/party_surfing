0b775c7bf8d509395936fc0696cf6b480e3be8b1
avitella <avitella@yandex-team.ru>
2014-12-13 01:33:27 +0300

    added login tests

--- test/controllers/session_controller_test.rb
+++ test/controllers/session_controller_test.rb
@@ -22,4 +22,28 @@ class SessionControllerTest < ActionController::TestCase
     click_on "Выйти"
     assert_equal "/", current_path
   end
+
+  test "login wrong password" do
+    visit "/users/sign_in"
+    fill_in "Email", with: "lol@lol.lol"
+    fill_in "Password", with: "lolkalolkaa"
+    click_on "Log in"
+    assert_equal "/users/sign_in", current_path
+  end
+
+  test "login wrong email right password" do
+    visit "/users/sign_in"
+    fill_in "Email", with: "lol@lol.lola"
+    fill_in "Password", with: "lolkalolka"
+    click_on "Log in"
+    assert_equal "/users/sign_in", current_path
+  end
+
+  test "login wrong email wrong password" do
+    visit "/users/sign_in"
+    fill_in "Email", with: "lol@lol.lola"
+    fill_in "Password", with: "lolkalolkaa"
+    click_on "Log in"
+    assert_equal "/users/sign_in", current_path
+  end
 end
