7f6faa5b692cda938a819136645bcdecee074b33
avitella <avitella@yandex-team.ru>
2014-12-13 00:04:04 +0300

    added test

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,6 +4,12 @@ class WelcomeControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-  end
 
+    assert_select "a" do
+      assert_select "img"
+    end
+    assert_select "a[href=/users/sign_in]"
+    assert_select "a[href=/users/sign_up]"
+    assert_select "a[href=/parties]"
+  end
 end
