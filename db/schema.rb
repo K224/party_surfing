39dcf374e639cc7d4a15fe9d41f067554afdc617
Siend <Siend.ru@gmail.com>
2014-12-17 21:03:34 +0300

    Edit Sign up, Log in, Forrgot password

--- db/schema.rb
+++ db/schema.rb
@@ -46,22 +46,6 @@ ActiveRecord::Schema.define(version: 20141211084050) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
-  create_table "ckeditor_assets", force: true do |t|
-    t.string   "data_file_name",               null: false
-    t.string   "data_content_type"
-    t.integer  "data_file_size"
-    t.integer  "assetable_id"
-    t.string   "assetable_type",    limit: 30
-    t.string   "type",              limit: 30
-    t.integer  "width"
-    t.integer  "height"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
-  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
-
   create_table "comments", force: true do |t|
     t.text     "comment"
     t.integer  "commentable_id"
@@ -114,7 +98,7 @@ ActiveRecord::Schema.define(version: 20141211084050) do
     t.string   "name",                default: "Anonymous"
     t.string   "surname",             default: "Anonymous"
     t.string   "photo"
-    t.date     "birthday",            default: '2014-10-16'
+    t.date     "birthday",            default: '2014-12-17'
     t.text     "contacts"
     t.integer  "user_id"
     t.datetime "created_at"
