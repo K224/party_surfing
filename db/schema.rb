749fb7c61b87f1f834d78d431c2a636fe5e1fc7f
diplay <diplaySuper@gmail.com>
2015-03-13 21:24:39 +0300

    coordinates fix #102

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303102144) do
+ActiveRecord::Schema.define(version: 20150308155121) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -98,7 +98,7 @@ ActiveRecord::Schema.define(version: 20150303102144) do
     t.string   "name",                default: "Anonymous"
     t.string   "surname",             default: "Anonymous"
     t.string   "photo"
-    t.date     "birthday",            default: '2014-10-16'
+    t.date     "birthday",            default: '2015-03-03'
     t.text     "contacts"
     t.integer  "user_id"
     t.datetime "created_at"
@@ -164,5 +164,20 @@ ActiveRecord::Schema.define(version: 20150303102144) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: true do |t|
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.integer  "voter_id"
+    t.string   "voter_type"
+    t.boolean  "vote_flag"
+    t.string   "vote_scope"
+    t.integer  "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
 
 end
