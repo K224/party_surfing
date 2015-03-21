579709ef058e8d0c6dabb7ec1a3a31f24ecee8f2
Avitella <urtashev@gmail.com>
2015-03-21 10:49:53 +0000

    Remove old tests and added tests with description

--- test/controllers/session_controller_test.rb
+++ test/controllers/session_controller_test.rb
@@ -3,75 +3,25 @@
 require 'test_helper'
 
 class SessionControllerTest < ActionController::TestCase
-  test "sign_in button" do
-    visit "/users/sign_in"
+  test "Check header after login" do
+    puts "
+      5. Изменение шапки сервиса на главном экране после аутентификации.
+        Проверяемое тест требование:
+          1.1.1.2
+        Шаги:
+          1. Зайти на сайт(http://test-party.herokuapp.com/)
+          2. Нажать на кнопку «Вход»
+          3. Войти под учетной записью пользователя (логин – lol@lol.lol,пароль – lalkalalka)
+        Ожидаемый результат:
+          1. После нажатия на кнопку «Вход» осуществляется переход на страницу входа.
+          2. После входа под соответствующей учетной записью осуществляется переход на главную страницу, где вместо кнопок «Вход» и «Зарегистрироваться» на странице расположены кнопки «Профиль» и «Выйти»."
+    visit "/"
     click_on "Вход"
-    assert_equal "/users/sign_in", current_path
-  end
-
-  test "sign_up button" do
-    visit "/users/sign_in"
-    click_on "Зарегистрироваться"
-    assert_equal "/users/sign_up", current_path
-  end
-
-  test "login" do
-    visit "/users/sign_in"
     fill_in "Email", with: "lol@lol.lol"
     fill_in "Password", with: "lolkalolka"
     click_on "Log in"
-    assert_equal "/", current_path
+    assert has_link? "Профиль"
+    assert has_link? "Выйти"
     click_on "Выйти"
-    assert_equal "/", current_path
-  end
-
-  test "login wrong password" do
-    visit "/users/sign_in"
-    fill_in "Email", with: "lol@lol.lol"
-    fill_in "Password", with: "araaraara"
-    click_on "Log in"
-    assert_equal "/users/sign_in", current_path
-  end
-
-  test "login wrong email right password" do
-    visit "/users/sign_in"
-    fill_in "Email", with: "lol@lol.lola"
-    fill_in "Password", with: "lolkalolka"
-    click_on "Log in"
-    assert_equal "/users/sign_in", current_path
-  end
-
-  test "login wrong email wrong password" do
-    visit "/users/sign_in"
-    fill_in "Email", with: "lol@lol.lola"
-    fill_in "Password", with: "lolkalolkaa"
-    click_on "Log in"
-    assert_equal "/users/sign_in", current_path
-  end
-  
-  test "login witout email" do
-    visit "/users/sign_in"
-    fill_in "Password", with: "lolkalolkaa"
-    click_on "Log in"
-    assert_equal "/users/sign_in", current_path
-  end
-
-  test "login without password" do
-    visit "/users/sign_in"
-    fill_in "Email", with: "lol@lol.lola"
-    click_on "Log in"
-    assert_equal "/users/sign_in", current_path
-  end
-
-  test "login without all" do
-    visit "/users/sign_in"
-    click_on "Log in"
-    assert_equal "/users/sign_in", current_path
-  end
-
-  test "login partner links" do
-    visit "/users/sign_in"
-    assert has_link? "Sign in with Facebook"
-    assert has_link? "Sign in with Vkontakte"
   end
 end
