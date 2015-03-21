2946ca6d38f8f23d0f8a7b001d5ba04c91c7e3c1
Avitella <urtashev@gmail.com>
2015-03-21 10:17:55 +0000

    Added description for tests

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,18 +4,45 @@ require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
   test "sign_in button" do
+    puts "
+      2. Кнопка «Вход» на главном экране.
+        Проверяемое тест требование:
+          1.1.1.6
+        Шаги:
+          1. Зайти на сайт(http://test-party.herokuapp.com/)
+          2. Нажать на кнопку «Вход»
+        Ожидаемый результат:
+          Осуществляется переход на страницу входа."
     visit "/"
     click_on "Вход"
     assert_equal "/users/sign_in", current_path
   end
 
   test "sign_up button" do
+    puts "
+      3. Кнопка «Зарегистрироваться» на главном экране.
+        Проверяемое тест требование:
+          1.1.1.5
+        Шаги:
+          1. Зайти на сайт(http://test-party.herokuapp.com/)
+          2. Нажать на кнопку «Зарегистрироваться»
+        Ожидаемый результат:
+          Осуществляется переход на страницу регистрации пользователей."
     visit "/"
     click_on "Зарегистрироваться"
     assert_equal "/users/sign_up", current_path
   end
 
   test "let's party button" do
+    puts "
+      4. Кнопка «Let’sParty» на главном экране.
+        Проверяемое тест требование:
+          1.1.1.4
+        Шаги:
+          1. Зайти на сайт(http://test-party.herokuapp.com/)
+          2. Нажать на кнопку «Let’sParty»
+        Ожидаемый результат:
+          Осуществляется переход на страницупоиска мероприятий."
     visit "/"
     click_on "Let's Party"
     assert_equal "/parties", current_path
