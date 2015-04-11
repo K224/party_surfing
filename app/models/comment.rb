4163e7fa442788bbfd74c34935a74b87f9665376
diplay <diplaySuper@gmail.com>
2015-04-11 13:56:16 +0300

    fixed #166

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,10 +6,19 @@ class Comment < ActiveRecord::Base
 
   default_scope -> { order('created_at ASC') }
 
+  validate :validate_comment
+
   # NOTE: install the acts_as_votable plugin if you
   # want user to vote on the quality of comments.
   #acts_as_voteable
 
   # NOTE: Comments belong to a user
   belongs_to :user
+
+protected
+  def validate_comment
+    if comment.length > 2000 || comment.length < 1
+      errors.add(:comment);
+    end
+  end
 end
